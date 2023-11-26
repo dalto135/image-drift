21f4e136cf97177bebd1d9b4df74c244052d5695
dalto135 <67397294+dalto135@users.noreply.github.com>
2023-11-26 18:02:19 -0500

    got api calls working using fetch

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-import OpenAI from "openai";
-
+const dall_e_url = 'https://api.openai.com/v1/images/generations';
+const vision_url = 'https://api.openai.com/v1/chat/completions';
 
 let prompts = [];
 let images = [];
 
-let imagesDiv = document.querySelector("#imagesDiv");
-let promptsDiv = document.querySelector("#promptsDiv");
-
 let promptInputDiv = document.querySelector("#promptInput");
 let apiKeyInputDiv = document.querySelector("#apiKeyInput");
 
-console.log("hello world");
+function generate() {
+    console.log();
+    console.log("GENERATING!!");
 
-async function generate() {
-    console.log("GENERATE FUNCTION");
     let prompt = promptInputDiv.value;
-
     if (prompts.length == 0) {
         prompts.push(prompt);
     }
 
-    let apiKey = apiKeyInputDiv.value;
+    dall_e_call(prompt);
+}
 
-    try {
-        const dall_e_response = await openai.images.generate({
+function dall_e_call(prompt) {
+    fetch(dall_e_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKeyInputDiv.value}`
+        },
+        body: JSON.stringify({
             model: "dall-e-2",
             // prompt: "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: " + prompt,
             prompt: prompt,
             n: 1,
             size: "1024x1024",
-        });
-
-        const image = dall_e_response.data[0].url;
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        let image = data.data[0].url;
         images.push(image);
-
-        if (images) {
-            imagesDiv.innerHTML = '<div style="width: 50%; aspect-ratio: 2 / 1;"></div>';
-
-            images.forEach(image => {
-                imagesDiv.innerHTML += '<img src=' + image + ' style="width: 50%;"></img>';
-            })
-
-            imagesDiv.innerHTML += '<br/>';
-        }
-        
+        displayImages();
 
         console.log();
-        console.log("IMAGE URL:");
+        console.log("IMAGE");
         console.log(image);
 
-        const vision_response = await openai.chat.completions.create({
-            model: "gpt-4-vision-preview",
+        vision_call(image);
+    })
+    .catch(error => console.error(error));
+}
+
+function vision_call(image) {
+    fetch(vision_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKeyInputDiv.value}`
+        },
+        body: JSON.stringify({
             messages: [
                 {
                     role: "user",
@@ -68,38 +74,49 @@ async function generate() {
                     ],
                 },
             ],
+            model: 'gpt-4-vision-preview',
             max_tokens: 300
-        });
-
-        prompt = vision_response.choices[0].message.content;
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        let prompt = data.choices[0].message.content;
         prompts.push(prompt);
 
-        if (prompts) {
-            prompts.forEach(prompt => {
-                promptsDiv.innerHTML += '<textarea readonly style="width: 50%; aspect-ratio: 1 / 1; font-size: large;">' + prompt + '</textarea>';
-            })
-            promptsDiv.innerHTML += '<br/>';
-        }
+        displayPrompts();
+
+        promptInputDiv.value = prompts[prompts.length - 1];
 
         console.log();
-        console.log("IMAGE PROMPT:");
+        console.log("PROMPT");
         console.log(prompt);
-        console.log();
-    } catch (error) {
-        console.log();
-        console.error("Error generating image:", error);
-        console.log();
-    }
+    })
+    .catch(error => console.error(error));
+}
 
-    promptInputDiv.value = prompts[prompts.length - 1];
-    apiKeyInputDiv.value = apiKey;
+function displayImages() {
+    let imagesDiv = document.querySelector("#imagesDiv");
+
+    let imagesDivList = '<div style="width: 50%; aspect-ratio: 2 / 1;"></div>';
+    images.forEach(image => {
+        imagesDivList += '<img src=' + image + ' style="width: 50%;"></img>';
+    });
+    imagesDivList += '<br/>';
+
+    imagesDiv.innerHTML = imagesDivList;
+}
+
+function displayPrompts() {
+    let promptsDiv = document.querySelector("#promptsDiv");
+
+    let promptDivList = "";
+    prompts.forEach(prompt => {
+        promptDivList += `<textarea readonly style="width: 50%; aspect-ratio: 1 / 1; font-size: large;">${prompt}</textarea>`;
+    });
+    promptDivList += '<br/>';
+
+    promptsDiv.innerHTML = promptDivList;
 }
 
 let generateButton = document.querySelector('#generate');
-
-function test() {
-    promptInputDiv.value = "hello world";
-    console.log("HELLO WORLD");
-}
-
-generateButton.addEventListener('click', test);
+generateButton.addEventListener('click', generate);
